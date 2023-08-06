4e5aff3c9bf8f53e5f6bb8f24c4d799917a1decf
Phil Sainty <phil@catalyst.net.nz>
2023-08-07 00:51:16 +1200

    WIP: Old events can't be loaded after joining a room
    See #148

--- ement-room.el
+++ ement-room.el
@@ -1436,16 +1436,17 @@ see."
                        (list (read-number "Number of messages: "))
                        (number current-prefix-arg))
                      :buffer (current-buffer)))
-  (unless ement-room-retro-loading
+  (unless (or ement-room-retro-loading (not room))
     (pcase-let* (((cl-struct ement-room id prev-batch) room)
                  (endpoint (format "rooms/%s/messages" (url-hexify-string id))))
       ;; We use a timeout of 30, because sometimes the server can take a while to
       ;; respond, especially if loading, e.g. hundreds or thousands of events.
       (ement-api session endpoint :timeout 30
-        :params (list (list "from" prev-batch)
-                      (list "dir" "b")
-                      (list "limit" (number-to-string number))
-                      (list "filter" (json-encode ement-room-messages-filter)))
+        :params (nconc (and prev-batch
+                            (list (list "from" prev-batch)))
+                       (list (list "dir" "b")
+                             (list "limit" (number-to-string number))
+                             (list "filter" (json-encode ement-room-messages-filter))))
         :then then
         :else (lambda (plz-error)
                 (when buffer
